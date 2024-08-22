ac4eac690fbd71475ea6f6f0671b65b6103360ff
Niklas Widmann <nik@somethingcatchy.net>
2024-08-22 12:01:42 +0200

    add mod dump

--- common/src/main/java/com/possible_triangle/registry_dump/command/DumpCommand.java
+++ common/src/main/java/com/possible_triangle/registry_dump/command/DumpCommand.java
@@ -1,9 +1,12 @@
 package com.possible_triangle.registry_dump.command;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.possible_triangle.registry_dump.CommonClass;
+import com.possible_triangle.registry_dump.Services;
+import com.sun.jdi.connect.Connector;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.commands.arguments.ResourceKeyArgument;
@@ -20,6 +23,12 @@ public class DumpCommand {
                         .executes(ctx -> dumpRegistries(ctx, $ -> true))
                         .then(Commands.argument("type", ResourceKeyArgument.key(Registry.REGISTRY.key()))
                                 .executes(ctx -> dumpRegistries(ctx, createArgumentPredicate(ctx)))
+                        )
+                )
+                .then(Commands.literal("mods")
+                        .executes(ctx -> dumpMods(ctx, true))
+                        .then(Commands.argument("simple", BoolArgumentType.bool())
+                                .executes(ctx -> dumpMods(ctx, BoolArgumentType.getBool(ctx, "simple")))
                         )
                 )
         );
@@ -42,6 +51,16 @@ public class DumpCommand {
         }
 
         return registries.size();
+    }
+
+    private static int dumpMods(CommandContext<CommandSourceStack> context, boolean simple) throws CommandSyntaxException {
+        final var server = context.getSource().getServer();
+        final var mods = Services.PLATFORM.collectMods().toList();
+        final var emitter = CommonClass.getDump(server);
+
+        emitter.dump(mods, simple);
+
+        return mods.size();
     }
 
 }
