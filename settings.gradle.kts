eafa7031618f89605639e24e91e018a723c30f7e
Niklas Widmann <nik@somethingcatchy.net>
2026-04-06 11:37:10 +0200

    update toolchain

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
 }
 
 plugins {
-    id("com.possible-triangle.helper") version ("1.0.57")
+    id("com.possible-triangle.helper") version ("1.2")
 }
 
 include("common", "fabric", "neoforge", "forge")
